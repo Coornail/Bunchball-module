2a18602fa475bbe152830f8818f60fbd6366bf46
Kornel Lugosi <coornail@gmail.com>
2012-07-25 14:28:03 +0200

    Implement comment recieving trigger

--- modules/bunchball_entities/plugins/bunchball_entities/BunchballEntitiesDefault.class.php
+++ modules/bunchball_entities/plugins/bunchball_entities/BunchballEntitiesDefault.class.php
@@ -87,9 +87,8 @@ class BunchballEntitiesDefault implements BunchballPluginInterface, BunchballEnt
    *    operation to register (ie: insert / update / comment)
    */
   public function send($id, $type, $user, $op) {
-    if (in_array($op, array('insert', 'update', 'comment')) && $this->checkSend($id, $op)) {
+    if (in_array($op, array('insert', 'update', 'comment', 'comment_receive')) && $this->checkSend($id, $op)) {
       try {
-        // log in
         $this->nitro->drupalLogin($user);
         $action = $this->getActionName($id, $op);
         $this->nitro->logAction($action);
