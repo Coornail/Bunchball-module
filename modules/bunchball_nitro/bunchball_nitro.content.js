945ce59a1579025caa5401308e89085e2c385cbf
Gergely Tamas Kurucz <temaruk@gmail.com>
2012-07-31 17:27:54 +0200

    Refactored part of the client side action logging to make Youtube actions actually working
    The callback which is called by the JS API of a loaded Youtube video must be defined before the video loads, this required separating the connected methods into its own file for inclusion in the 'header' scope. Then the whole client side queue mechanism had to be improved so that the queue can be populated with Youtube actions that need processing prior to having the Nitro library fully loaded.

--- modules/bunchball_nitro/bunchball_nitro.content.js
+++ modules/bunchball_nitro/bunchball_nitro.content.js
@@ -1,3 +1,6 @@
+Drupal.bunchball = Drupal.bunchball || {};
+Drupal.bunchball.userCommandsArray = Drupal.bunchball.userCommandsArray || [];
+
 (function($) {
   Drupal.behaviors.bunchballNitroContent = {
     attach: function (context, settings) {
@@ -9,9 +12,6 @@
 }) (jQuery);
 
 var _currentUserId = '';
-var _userCommandsArray = [];
-var _thePlayer = '';
-var _playerPlayed = 0;
 
 function setUserId() {
   var userId = Drupal.bunchball.nitro.connectionParams.userId;
@@ -40,7 +40,7 @@ function userViewedContent() {
     inObj.uid = _currentUserId;
     inObj.tags = sentTags;
     inObj.ses = '';
-    _userCommandsArray.push(inObj);
+    Drupal.bunchball.userCommandsArray.push(inObj);
   }
 
   if (nodeData.viewReceiveAction) {
@@ -50,116 +50,10 @@ function userViewedContent() {
     inObj.uid = nodeData.nodeUID;
     inObj.tags = sentTags;
     inObj.ses = '';
-    _userCommandsArray.push(inObj);
+    Drupal.bunchball.userCommandsArray.push(inObj);
   }
 
-  nitroIterateQueue();
-}
-
-function submitNitroAPICall(tags) {
-  var params = new Array();
-
-  params[0] = 'method=' + encodeURIComponent('user.logAction');
-  params[1] = 'sessionKey=' + encodeURIComponent(_userCommandsArray[0].ses);
-  params[2] = 'tags=' + encodeURIComponent(tags);
-
-  var queryString = params.join('&');
-
-  nitroCallback("data", "token");
-  Drupal.bunchball.nitro.callAPI(queryString, "nitroCallback");
-}
-
-function nitroCallback(data, token) {
-  // remove from array
-  if (_userCommandsArray.length > 0) {
-    _userCommandsArray.splice(0, 1);
-  }
-
-  nitroIterateQueue();
-}
-
-function nitroLogin(userId) {
-  var connectionParams = Drupal.bunchball.nitro.connectionParams;
-  var params = new Array();
-
-  params[0] = 'method=' + encodeURIComponent('user.login');
-  params[1] = 'apiKey=' + encodeURIComponent(connectionParams.apiKey);
-  params[2] = 'userId=' + encodeURIComponent(userId);
-  params[3] = 'ts=' + encodeURIComponent(connectionParams.timeStamp);
-  params[4] = 'sig=' + encodeURIComponent(connectionParams.signature);
-
-  var loginQuery = params.join('&');
-
-  Drupal.bunchball.nitro.callAPI(loginQuery, "nitroLoginCallback");
-}
-
-function nitroLoginCallback(data, token) {
-  // this is a stub that can be used later to track responses from the server.
-  _userCommandsArray[0].ses = data['Nitro']['Login']['sessionKey'];
-
-  // do the nitro API call..
-  submitNitroAPICall(_userCommandsArray[0].tags);
-}
-
-
-function nitroIterateQueue() {
-  // proceed to log in next user
-  if (_userCommandsArray.length > 0) {
-    nitroLogin(_userCommandsArray[0].uid);
-  }
-}
-
-/**
- * Your HTML pages that display the chromeless player must implement
- * a callback function named onYouTubePlayerReady.
- *
- * The API will call this function when the player is fully loaded
- * and the API is ready to receive calls.
- *
- * https://developers.google.com/youtube/js_api_reference#EventHandlers
- */
-function onYouTubePlayerReady(playerId) {
-  // attach the listener
-  (function ($) {
-    $("div.oembed-video .oembed-content object embed").each(function(i){
-      _thePlayer = this;
-      _thePlayer.addEventListener("onStateChange", nitroVideoStateChange);
-    });
-  }) (jQuery);
-}
-
-/**
- * Youtube video event listeners.
- *
- * https://developers.google.com/youtube/js_api_reference#Events
- */
-function nitroVideoStateChange(newState) {
-  // empty action to prevent anything from going forward
-  var youtubeData = Drupal.settings.bunchballNitroYoutube;
-  var action = "";
-
-  if (newState == 0) {
-    // Video ended.
-    action = youtubeData.artistEnd;
-    _playerPlayed = 0;
-  } else if (newState == 1 && _playerPlayed == 0) {
-    // Video started.
-    action = youtubeData.artistStart;
-    _playerPlayed = 1;
-  }
-
-  // only continue if there is something in Action
-  if (action.length > 1) {
-    action = action + ",Artist: " + youtubeData.artistName + ", Category: " + youtubeData.artistCategory;
-
-    var inObj = {};
-    inObj.uid = _currentUserId;
-    inObj.tags = action;
-    inObj.ses = '';
-    _userCommandsArray.push(inObj);
-
-    nitroIterateQueue();
-  }
+  nodeWaitForBunchballNitroInit();
 }
 
 function nitroSocialShareClicked(network) {
@@ -170,8 +64,17 @@ function nitroSocialShareClicked(network) {
     inObj.uid = _currentUserId;
     inObj.tags = action;
     inObj.ses = '';
-    _userCommandsArray.push(inObj);
+    Drupal.bunchball.userCommandsArray.push(inObj);
 
-    nitroIterateQueue();
+    Drupal.bunchball.WorkerQueue.nitroIterateQueue();
+  }
+}
+
+function nodeWaitForBunchballNitroInit() {
+  if (typeof Drupal.bunchball.nitro !== 'undefined' && typeof Drupal.bunchball.WorkerQueue.nitroIterateQueue === 'function') {
+    Drupal.bunchball.WorkerQueue.nitroIterateQueue();
+  }
+  else {
+    setTimeout(nodeWaitForBunchballNitroInit, 1500);
   }
 }
